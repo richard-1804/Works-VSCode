aa749983dfcaea137008bef68312902b89d6d908
richard-1804 <daniel.r.santos6@ba.estudante.senai.br>
2025-09-19 19:30:35 -0300

    Formatei os parênteses e ajustei a saída

--- cartao_de_visita.js
+++ cartao_de_visita.js
@@ -1,15 +1,3 @@
-/*const nomeCompleto = "Daniel Richard";
-let cargoEmpresa = "Sou parte da equipe que fundou a InovaTech e atuo como CyberSecurity";  
-let nomeEmpresa = "InovaTech";
-let emailContato = "inovatechempresarial@gmail.com";
-let telContato = "+55 (71) 99999-9999";
-let anosExperiencia = 5;
-let enderecoFisico = "Em frente ao Shopping Salvador Norte, Salvador - BA";
-let temFiliais = false;
-
-console.log("Nome Completo: ", nomeCompleto, "\nCargo na Empresa: ", cargoEmpresa , "\nNome da Empresa: ", nomeEmpresa, "\nE-mail para Contato: ", emailContato, "\nTelefone para Contato: ", telContato, "\nAnos de Experiência: ", anosExperiencia, "\nEndereço Físico: ", enderecoFisico, "\nPossui Filiais? ", temFiliais);
-*/
-
 const readline = require("readline");
 
 const r1 = readline.createInterface({
@@ -23,16 +11,16 @@ console.log("Olá, bem-vindo ao sistema de criação do seu cartão de visitas!!
 console.log("Insira seus dados abaixo:\n");
 
 r1.question("Qual o seu nome Completo? ", (nomePessoal) => {
-  dados.nomePessoal = nomePessoal;
+  dados.nomePessoal = (nomePessoal);
 
   r1.question("Qual o nome da sua empresa? ", (nomeEmpresa) => {
-    dados.nomeEmpresa = nomeEmpresa;
+    dados.nomeEmpresa = (nomeEmpresa);
 
     r1.question("Qual o seu cargo na empresa ", (cargo) => {
-      dados.cargo = cargo;
+      dados.cargo = (cargo);
 
       r1.question("Qual o e-mail da empresa? ", (emailEmpresa) => {
-        dados.emailEmpresa = emailEmpresa;
+        dados.emailEmpresa = (emailEmpresa);
 
         r1.question("Qual o telefone de contato? ", (telContato) => {
           dados.telContato = Number(telContato);
@@ -41,20 +29,23 @@ r1.question("Qual o seu nome Completo? ", (nomePessoal) => {
             dados.anoExp = Number(anoExp);
 
             r1.question("Qual o endereço físico (se tiver)? ", (endereco) => {
-              dados.endereco = endereco;
+              dados.endereco = (endereco);
 
               r1.question("A empresa possui filiais? ", (filiais) => {
-                dados.filiais = filiais;
+                dados.filiais = (filiais);
 
+
+                
+                console.log("\n---Seus Dados Pessoais---\n");
                 console.log(
                   "Seu nome: " + nomePessoal,
-                  "Nome da empresa: " + nomeEmpresa,
-                  "Seu cargo: " + cargo,
-                  "E-mail empresarial: " + emailEmpresa,
-                  "Telefone empresarial: " + telContato,
-                  "Anos de experiência: " + anoExp,
-                  "Endereço: " + endereco,
-                  "A empresa tem filiais? " + filiais
+                  "\nNome da empresa: " + nomeEmpresa,
+                  "\nSeu cargo: " + cargo,
+                  "\nE-mail empresarial: " + emailEmpresa,
+                  "\nTelefone empresarial: " + telContato,
+                  "\nAnos de experiência: " + anoExp,
+                  "\nEndereço: " + endereco,
+                  "\nA empresa tem filiais? " + filiais
                 );
 
                 r1.close();
